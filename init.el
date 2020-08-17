b4d2064193530f47b8846de15624b16744843015
Ismael Oliva <isma.oliva@gmail.com>
2020-08-17 20:47:09 +0100

    Farewell mac

--- init.el
+++ init.el
@@ -74,7 +74,7 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       ;; eshell            ; a consistent, cross-platform shell (WIP)
+       eshell            ; a consistent, cross-platform shell (WIP)
        ;;shell             ; a terminal REPL for Emacs
        ;;term              ; terminals in Emacs
        vterm             ; another terminals in Emacs
@@ -93,7 +93,6 @@
         +dictionary +offline
         )
        lsp
-       macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;; pass              ; password manager for nerds
